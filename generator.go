31f34167a1c13077f619700c3b6cb73231f5d2c8
astup <andrii.stupnytskyi@netdocuments.com>
2019-03-14 19:44:26 +0200

    Rewrited generator according to hints that were discussed

--- generator.go
+++ generator.go
@@ -2,108 +2,97 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
-	"sort"
-
-	"github.com/gorilla/mux"
 )
 
-// gometalinter
-type PasswordConfig struct { //name fields start with Upper case
-	minLength int `json:"min_length"`
-	specialCharsAmount int `json:"special_chars_amount"`
-	numberAmount int `json:"number_amount"` //json: not nesessery
+var SpecialChars = []rune("[{}\"/$\\_`~&+,:;=?@#|'<>.-^*()%!]")
+var Numbers = []rune("1234567890")
+var Letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+var Vowels = []rune("AEIOUYaeiouy")
+
+type PasswordConfig struct {
+	MinLength          int
+	SpecialCharsAmount int
+	NumberAmount       int
 }
 
-type ResponseMessage struct { //name fields start with Upper case
-	originalPassword string
-	transformPassword string
+type Response struct {
+	GeneratedPassword string
 }
 
-func getPasswordConfigs(w http.ResponseWriter, r *http.Request)  {
+func getPasswordConfigs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var passwordConfigs PasswordConfig
-	if err := json.NewDecoder(r.Body).Decode(&passwordConfigs); err != nil {
-		// return error to w 400
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
 	}
-	response := generatePassword(passwordConfigs)
-	resp := json.NewEncoder(w).Encode(response)
+	if err := json.Unmarshal(body, &passwordConfigs); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := Response{
+		generatePassword(passwordConfigs),
+	}
+
+	resp, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	if _, err := w.Write(resp); err != nil {
-		// internal server error 500
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	w.Header().Set("Content-Type", "application/json")
 }
-//pass passwordConfig by value
-func generatePassword(configs PasswordConfig) ResponseMessage {
-	var originalPassword string
-	// remove numberAmount, specialCharsAmount
-	numberAmount := configs.numberAmount
-	specialCharsAmount :=  configs.specialCharsAmount
-	loopRange := configs.minLength + configs.numberAmount + configs.specialCharsAmount + rand.Intn(20)
 
-	//initialize slise of rune as a result 
+func generatePassword(configs PasswordConfig) string {
+	var result = []rune("")
+
+	for i := 0; i < configs.NumberAmount; i++ {
+		result = append(result, getRandomValue(Numbers))
+	}
+
+	for i := 0; i < configs.SpecialCharsAmount; i++ {
+		result = append(result, getRandomValue(SpecialChars))
+	}
+
+	loopRange := configs.MinLength + rand.Intn(20)
 	for i := 0; i < loopRange; i++ {
-		//change functions into one
-		//get original set as a password, remove vowels and shuffle
-		//change shuffle design
-		checkNumAmount(&numberAmount, &originalPassword)
-		checkSpecialChars(&specialCharsAmount, &originalPassword)
-		addLetters(&originalPassword)
+		result = append(result, getRandomValue(Letters))
 	}
 
-	var response ResponseMessage
-	response.originalPassword = originalPassword
-	//initialize this slise as global slise of rune
-	// map[int]struct{}{1:struct{}{},} 
-	vowels := []int{65, 69, 73, 79, 85, 89, 97, 101, 105, 111, 117, 121}
-	var transformPassword string
-
-	for _, v := range originalPassword {
-		if v == int32(sort.SearchInts(vowels, int(v))) && rand.Intn(2) != 0 {
-			transformPassword += fmt.Sprint(getValueFromRange(0, 9))
-		} else {
-			transformPassword += fmt.Sprintf("%c", v)
+	for i := 0; i < len(result); i++ {
+		for j := 0; j < len(Vowels); j++ {
+			if result[i] == Vowels[j] {
+				result[i] = getRandomValue(Numbers)
+			}
 		}
 	}
 
-	response.transformPassword = transformPassword
-
-	return response
+	return string(shuffleSlice(result))
 }
 
-func checkNumAmount(numAmount *int, password *string) {
-	if *numAmount > 0 && rand.Intn(2) != 0 {
-		*password += fmt.Sprintf("%c", getValueFromRange(48, 57))
-		*numAmount--
+func getRandomValue(runeSlice []rune) rune{
+	return runeSlice[rand.Intn(len(runeSlice))]
+}
+
+func shuffleSlice(runeSlice []rune) []rune {
+	for i := 0; i < len(runeSlice); i++ {
+		tmp := runeSlice[rand.Intn(len(runeSlice))]
+		tmp, runeSlice[i] = runeSlice[i], tmp
 	}
+	return runeSlice
 }
 
-func checkSpecialChars(charsAmount *int, password *string)  {
-	if *charsAmount > 0 && rand.Intn(2) != 0 {
-		switch rand.Intn(4) {
-		case 0: *password += fmt.Sprintf("%c", getValueFromRange(33, 47))
-		case 1: *password += fmt.Sprintf("%c", getValueFromRange(58, 64))
-		case 2: *password += fmt.Sprintf("%c", getValueFromRange(91, 96))
-		case 3: *password += fmt.Sprintf("%c", getValueFromRange(123, 126))
-		}
-		*charsAmount--
-	}
-}
-
-func addLetters(password *string)  {
-	switch rand.Intn(2) {
-	case 0: *password += fmt.Sprintf("%c", getValueFromRange(65, 90))
-	case 1: *password += fmt.Sprintf("%c", getValueFromRange(97, 122))
-	}
-}
-
-func getValueFromRange(min int, max int) int {
-	return rand.Intn(max - min) + min
-}
-
-func main()  {
+func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/generate", getPasswordConfigs).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", router))
